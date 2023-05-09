cce42ead54d46fdb771b6843509ce0020e6d73e9
Domae-back-end <ty_ty123@naver.com>
2023-05-10 01:05:26 +0900

    update: main url 수정

--- controller/register.py
+++ controller/register.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 register = Blueprint('register', __name__)
 
 
-@register.route('/register')
+@register.route('/')
 def register_page():
     return render_template('/user/index.html')
